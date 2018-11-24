950e56845af78e1bcf3c309c158fa9c06a7fbc3e
kai <arklaza_uva@hotmail.com>
2018-11-24 13:08:52 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: ''
+    }
+  },
+  sockets: {
+    connect: function () {
+      console.log('socket connected')
+    },
+    message: function (val) {
+      alert('Mensaje de bienvenida: ' + val.message)
+      this.message = val.message
+      console.log('this method fired by socket server. eg: io.emit("customEmit", data)')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,25 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'ws://localhost:3000'
+}))
+
+/* Vue.use(new VueSocketio({
+  debug: true,
+  connection: 'ws://localhost:3001'
+})) */
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
